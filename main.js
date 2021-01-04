fc8bb1589d042a499a7151b090a0d45725af185c
petarPAN <petarburic554@gmail.com>
2021-01-04 02:40:00 +0100

    Delete css transition when sound is played

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
+const keys = document.querySelectorAll('.key');
+
 function playSound(event) {
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+
+    console.log(event);
 
     audio.play();
     audio.currentTime = 0;
     key.classList.add('play');
 }
 
+function removeSound(event) {
+    if (event.propertyName !== 'transform') return;
+    event.target.classList.remove('play')
+}
 
+/* transitionend event is fired when a CSS transition has completed.*/
+keys.forEach(i => i.addEventListener('transitionend', removeSound));
 window.addEventListener('keydown', playSound);
